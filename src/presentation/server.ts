fe1100b56541baf445982d35590a234e726102b5
alonso537 <adrianaalonso002mail.com>
2023-10-25 07:35:02 -0600

    adaptador pattern

---
+++ src/presentation/server.ts
@@ -0,0 +1,17 @@
+import { CronService } from "./cron/cron-service";
+
+export class Server {
+
+
+    public static start() {
+        console.log('Server Started...');
+        
+        CronService.createJob(
+            '*/5 * * * * *',
+            () => {
+                const date = new Date()
+                console.log('5 seconds', date)
+            }
+        )
+    }
+}
