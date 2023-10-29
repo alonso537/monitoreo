800ae8a5ae1390b7931445ca3bd791b83e8cdbad
alonso537 <adrianaalonso002mail.com>
2023-10-29 11:14:39 -0600

    log entity

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -3,7 +3,17 @@ interface CheckServiceUseCase {
     execute(url:string):Promise<boolean>;
 }
 
+type SuccesCallback = () => void;
+type ErrorCallback = (error: string) => void;
+
 export class CheckService implements CheckServiceUseCase {
+
+    constructor(
+        private readonly successCallback:SuccesCallback,
+        private readonly errorCallback:ErrorCallback
+    ){
+
+    }
 
 
     async execute(url:string): Promise<boolean> {
@@ -15,10 +25,12 @@ export class CheckService implements CheckServiceUseCase {
             }
 
             console.log(`${url} is ok`);
+            this.successCallback()
             
             return true;
         } catch (error) {
             console.log(`${error}`);
+            this.errorCallback(`${error}`)
             
             return false
         }
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -2,16 +2,14 @@ import { CheckService } from "../domain/use-cases";
 import { CronService } from "./cron/cron-service";
 
 export class Server {
+  public static start() {
+    console.log("Server Started...");
 
-
-    public static start() {
-        console.log('Server Started...');
-        
-        CronService.createJob(
-            '*/5 * * * * *',
-            () => {
-                new CheckService().execute('https://google.com')
-            }
-        )
-    }
+    CronService.createJob("*/5 * * * * *", () => {
+      new CheckService(
+        () => console.log("success"),
+        (error) => console.log(error)
+      ).execute("https://google.com");
+    });
+  }
 }
