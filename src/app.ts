0735e6728ca10dc3f91da97bfaddf6267cf00c5b
alonso537 <adrianaalonso002mail.com>
2023-10-29 12:31:04 -0600

    env and validation

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import { Server } from "./presentation/server"
 
+import { Server } from "./presentation/server"
 
 
 (async () => {
--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -5,8 +5,8 @@ interface CheckServiceUseCase {
     execute(url:string):Promise<boolean>;
 }
 
-type SuccesCallback = () => void;
-type ErrorCallback = (error: string) => void;
+type SuccesCallback = (() => void) | undefined;
+type ErrorCallback = ((error: string) => void) | undefined;
 
 export class CheckService implements CheckServiceUseCase {
 
@@ -31,7 +31,7 @@ export class CheckService implements CheckServiceUseCase {
 
             // console.log(`${url} is ok`);
             this.logrepository.saveLog(log)
-            this.successCallback()
+            this.successCallback && this.successCallback()
             
             return true;
         } catch (error) {
@@ -39,7 +39,7 @@ export class CheckService implements CheckServiceUseCase {
             const errorMessage = `${url} is not ok. ${error}`
             const log = new LogEntity(errorMessage, LogSeverityLevel.high)
             this.logrepository.saveLog(log)
-            this.errorCallback(errorMessage)
+            this.errorCallback && this.errorCallback(errorMessage)
             
             return false
         }
